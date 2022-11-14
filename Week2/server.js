5b68400a92cb0677e3714d9156db7d9c8c9a2f6a
ValtteriLento <valtteri.lento@gmail.com>
2022-11-14 13:05:14 +0200

    commit

--- Week2/server.js
+++ Week2/server.js
@@ -1,25 +1,18 @@
+let hello = {
+        msg: "Hello world"
+    }
+
 const express = require("express");
+const os = require("os");
 const app = express();
 const port = 3000;
+
+app.get("/hello", (req, res) => {
+    res.json(hello);
+})
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 })
 
 app.listen(port, () => console.log("Server running"));
-
-
-
-
-
-
-
-
-
-/*
-const http = require("http");
-
-http.createServer(function(req, res) {
-    res.write("Hello World!");
-    res.end();
-}).listen(3000);*/
