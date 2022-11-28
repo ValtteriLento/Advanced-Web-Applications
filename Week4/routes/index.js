06717496a854108dd70565550e1ef014251b9834
ValtteriLento <valtteri.lento@gmail.com>
2022-11-28 14:16:17 +0200

    commit

--- Week4/routes/index.js
+++ Week4/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Recipes' });
+});
+
+router.get('/recipe/:food', function(req, res, next) {
+  res.send({
+    name: req.params.food,
+    ingredients: ["frozen pizza base", "tomato sauce", "mozzarellas"],
+    instructions: ["preheat oven 225C", "slice mozzarellas", "put tomato sauce on pizza base", "put mozzarella slices on pizza", "bake for 15 min"]
+})
 });
 
 module.exports = router;
