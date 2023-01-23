fff6593288e637e059a33bcfa30ef5bd969027ef
ValtteriLento <valtteri.lento@gmail.com>
2023-01-23 17:11:05 +0200

    commit

--- Week8/server.js
+++ Week8/server.js
@@ -8,12 +8,27 @@ function getUserByUsername(username) {
     return users.find(user => user.username == username)
 }
 
+function getUserById(id) {
+    return users.find(user => user.id === id)
+}
+
+const initializePassport = require('./passport-config')
+initializePassport(passport, getUserByUsername, getUserById)
+
 let users = []
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(session({
+    secret: "ASF#LSAFK192409124saf&",
+    resave: false,
+    saveUninitialized: false
+}))
 
-app.post('/api/user/register', async (req, res) => {
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.post('/api/user/register', checkNotAuthenticated, async (req, res) => {
     if (!getUserByUsername(req.body.username)) {
         try {
             const hashedPassword = await bcrypt.hash(req.body.password, 10)
@@ -32,8 +47,30 @@ app.post('/api/user/register', async (req, res) => {
     }
 })
 
+app.post('/api/user/login', checkNotAuthenticated, passport.authenticate('local'), (req, res) => {
+    res.sendStatus(200)
+})
+
+app.get('/api/secret', checkAuthenticated, (req, res) => {
+    res.sendStatus(200)
+})
+
 app.get('/api/user/list', (req, res) => {
     res.send(users)
 })
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    res.sendStatus(401)
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect("/api/secret")
+    }
+    return next()
+}
 
 app.listen(3000)
