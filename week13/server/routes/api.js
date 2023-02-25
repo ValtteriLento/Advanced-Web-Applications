b3a84e3d5bcd124e64d4a7ef0cb6e04b11a8e23e
ValtteriLento <valtteri.lento@gmail.com>
2023-02-25 16:24:43 +0200

    commit

---
+++ week13/server/routes/api.js
@@ -0,0 +1,26 @@
+var express = require('express');
+const mongoose = require("mongoose");
+const Book = require("../models/Book");
+var router = express.Router();
+
+/* POST data. */
+router.post('/book', (req, res, next) => {
+    Book.findOne({ name: req.body.name}, (err, name) => {
+        if(err) return next(err);
+        if(!name) {
+            new Book({
+                name: req.body.name,
+                author: req.body.author,
+                pages: req.body.pages
+            }).save((err) => {
+                if(err) return next(err);
+                return res.send(req.body);
+            });
+        } else {
+            return res.status(403).send("Already has that book!");
+        }
+    })
+    res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
