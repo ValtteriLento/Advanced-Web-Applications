fa92ba7468d88a95d6c208087c46d8c5545ab150
ValtteriLento <valtteri.lento@gmail.com>
2023-02-26 15:43:07 +0200

    commit

--- week13/client/src/App.js
+++ week13/client/src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
+import {useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>books</h1>
-    </div>
-  );
+    const [bookData, setBookData] = useState({})
+
+    const submit = (e) => {
+        e.preventDefault()
+
+        fetch("/api/book", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(bookData),
+            mode: "cors"
+        })
+            .then(response => response.json())
+    }
+
+    const handleChange = (e) => {
+        setBookData({...bookData, [e.target.id]: e.target.value})
+    }
+
+    return (
+        <div className="App">
+            <h1>Books</h1>
+            <form onSubmit={submit} onChange={handleChange}>
+                <input id="name" type="string"></input>
+                <input id="author" type="string"></input>
+                <input id="pages" type="number"></input>
+                <input id="submit" type="submit"></input>
+            </form>
+        </div>
+    );
 }
 
 export default App;
--- week13/server/app.js
+++ week13/server/app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-const mongoose = require("mongoose");
 var logger = require('morgan');
+const mongoose = require("mongoose");
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +22,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+if (process.env.NODE_ENV === "development") {
+    var corsOptions = {
+        origin: "http://localhost:3000",
+        optionsSuccessStatus: 200,
+    };
+    app.use(cors(corsOptions));
+}
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
